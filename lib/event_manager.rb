070c14a6168be8146f6821c0a1d6cbd5128571d1
MountainMan <colemanrumsey@gmail.com>
2023-09-30 15:41:00 +0300

    added the day targeting. finished project

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -4,6 +4,7 @@ require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 require 'date'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
@@ -24,24 +25,22 @@ def clean_phone_number(phone_number)
   end
 end
 
-registration_dates = []
-
-def get_peak_hour(registration_dates)
-  # Convert registration times from string to DateTime objects
-registration_dates = registration_dates.map { |time_str| DateTime.strptime(time_str, '%Y-%m-%d %H:%M:%S') }
-
-# Group registrations by hour and count number of registrations for each hour
-hourly_registrations = registration_dates.group_by {&:hour}.map { |hour, registrations| [hour, registrations.length] }.to_h
-
-# Find the hour with the highest number of registrations
-peak_hour = hourly_registrations.max_by { |hour, count| count }[0]
-
-# Print results
-puts "Hourly registrations: #{hourly_registrations}"
-puts "Peak registration hour: #{peak_hour}"
+def clean_date(reg_date)
+  raw_date = DateTime.strptime(reg_date, '%m/%d/%Y %k:%M')
+  raw_date.strftime('%Y/%m/%d %k:%M')
 end
 
-peak_reg = get_peak_hour(registration_dates)
+# def get_peak_hour(registration_dates)
+#   # Convert registration times from string to DateTime objects
+# registration_dates = registration_dates.map { |time_str| DateTime.strptime(time_str, '%Y-%m-%d %H:%M:%S') }
+
+# # Group registrations by hour and count number of registrations for each hour
+# hourly_registrations = registration_dates.group_by {&:hour}.map { |hour, registrations| [hour, registrations.length] }.to_h
+
+# # Find the hour with the highest number of registrations
+# peak_hour = hourly_registrations.max_by { |hour, count| count }[0]
+
+# end
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
@@ -58,7 +57,7 @@ def legislators_by_zipcode(zip)
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -78,19 +77,42 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+day_hour_array = []
+weekday_array = []
 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  reg_date = row[:regdate]
+  fin_date = clean_date(reg_date)
+  time_format = '%Y/%m/%d %k:%M'
+  days = Date::DAYNAMES
+
+  puts "Fetching info for #{name}, ID ##{id}."
+  puts reg_date
+  puts fin_date
+
   zipcode = clean_zipcode(row[:zipcode])
+  phone = clean_phone_number(row[:homephone])
+
+  date = DateTime.strptime(reg_date, '%m/%d/%Y %k:%M')
+  puts date
+  weekday = days[date.wday]
+  weekday_array << weekday
+  day_hour_array << date.hour
+
+  puts "Phone number: #{phone}"
+  puts date
+  puts "Day of the week: #{weekday}"
+
   legislators = legislators_by_zipcode(zipcode)
-  phone = clean_phone_number(row[:HomePhone])
-  # Get the "registration_date" column from the current row
-  registration_date_str = row[:RegDate]
-  # Convert the string to a DateTime object and add it to the registration_dates array
-  registration_dates << DateTime.strptime(registration_date_str, '%Y-%m-%d %H:%M:%S')
-
   form_letter = erb_template.result(binding)
-
   save_thank_you_letter(id, form_letter)
+
+  puts "There's the info you wanted..."
+  puts ''
 end
+
+# Print results
+puts "Hour that most people registered: #{day_hour_array.max_by { |a| day_hour_array.count(a) }}h"
+puts "Day that most people registered: #{weekday_array.max_by { |a| weekday_array.count(a) }}"
